d9f6ca312a34fe1ba098dddd9efa0bc3a058ac2d
Eldar <satyndiev_e@auca.kg>
2023-06-19 23:32:40 +0600

    leetCode 1365 Accepted

---
+++ leetcod/src/p1365.java
@@ -0,0 +1,17 @@
+public class p1365 {
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] res = new int[nums.length];
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(nums[i] > nums[j]){
+                    count++;
+                }
+            }
+            res[i] += count;
+            count = 0;
+        }
+        return res;
+    }
+}
